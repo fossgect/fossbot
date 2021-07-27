11cbcff68061f58f42a4f0231cce5e3ff2aacd8e
Mr-Skully <emiltitus.inc@gmail.com>
2021-07-27 18:08:43 +0530

    Automate the command registering process

--- bot.js
+++ bot.js
@@ -1,19 +1,31 @@
+const fs = require("fs");
+require("dotenv").config();
+
 const Discord = require("discord.js");
 require("discord-reply");
 const client = new Discord.Client();
-require("dotenv").config();
 
-const messageHandler = require("./src/messageHandler");
+const { PREFIX, BOT_TOKEN } = require("./util/botConfig.");
+// const messageHandler = require("./src/messageHandler");
 
-client.on("ready", () => {
+client.once("ready", () => {
   console.log(`${client.user.tag} is ready`);
 });
-
+/*
 client.on("message", (message) => {
   if (message.author.bot) return;
-  //function to handle messages
+  // function to handle messages
   messageHandler(client, message);
 });
-
-//login
-client.login(process.env.BOT_TOKEN);
+*/
+// login
+const commandFolders = fs.readdirSync("./commands");
+for (const folder of commandFolders) {
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
+}
+client.prefix = PREFIX;
+client.login(BOT_TOKEN);
