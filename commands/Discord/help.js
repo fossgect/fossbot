8d60c162465f3116006751c7281f30747c40c496
Mr-Skully <emiltitus.inc@gmail.com>
2021-07-27 18:07:32 +0530

    Reorganize 'help' command

---
+++ commands/Discord/help.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "help",
+  description: "Shows this message.",
+  usage: "help",
+  category: "Discord",
+  // eslint-disable-next-line no-unused-vars
+  execute(message, args) {
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`fossbot commands`)
+      .setColor("#04c46c")
+      .attachFiles(["logo.png"])
+      .setThumbnail("attachment://logo.png");
+
+    embed.setDescription(`
+**General :**    
+  &help
+
+**Github :**    
+  &invite  username     
+  &remove  username _(admin only)_
+  `);
+
+    message.lineReply(embed);
+  },
+};
