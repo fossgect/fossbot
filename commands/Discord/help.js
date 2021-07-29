13be37349540a1bbc2126a14697671204aee52de
Mr-Skully <emiltitus.inc@gmail.com>
2021-07-29 17:01:20 +0530

    Set up a dynamic help command

--- commands/Discord/help.js
+++ commands/Discord/help.js
@@ -2,25 +2,41 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "help",
-    description: "Shows this message.",
-    usage: "help",
+    description: "Shows help for a specific command, or lists all available commands.",
+    usage: "[command]",
+    aliases: ["commands"],
     category: "Discord",
-    // eslint-disable-next-line no-unused-vars
     execute(message, args) {
         const embed = new Discord.MessageEmbed()
-            .setTitle(`fossbot commands`)
+            .setTitle("FossBot Help")
             .setColor("#04c46c")
             .attachFiles(["logo.png"])
-            .setThumbnail("attachment://logo.png");
-        embed.setDescription(`
-**General :**    
-  &help
-
-**Github :**    
-  &invite  username     
-  &remove  username _(admin only)_
-  `);
-
+            .setThumbnail("attachment://logo.png")
+            .setAuthor("FossBot🤖", "attachment://logo.png", "https://github.com/fossgect/fossbot/");
+        const reply = [];
+        const { commands } = message.client;
+        if (!args.length) {
+            reply.push("**Available Commands**");
+            reply.push(commands.map(command => `- ${command.name}`).join("\n"));
+            reply.push(`\nUse **${message.client.prefix}help [command-name]** to get help for the specific command.`);
+            reply.push(`Example: **${message.client.prefix}help help**`);
+        }
+        else {
+            const name = args[0].toLowerCase();
+            const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+            if (!command) {
+                reply.push("Uh-oh! That's not a valid command (yet😋).");
+                reply.push(`Use **${message.client.prefix}help** to get a list of available commands.`);
+            }
+            else{
+                reply.push(`**Name**: ${command.name}`);
+                if (command.category) reply.push(`**Category**: *${command.category}*`);
+                if (command.description) reply.push(`**Description**: ${command.description}`);
+                if (command.usage) reply.push(`**Usage**: ${message.client.prefix}${command.name} ${command.usage}`);
+                if (command.aliases) reply.push(`**Aliases:** ${command.aliases.join(", ")}`);
+            }
+        }
+        embed.setDescription(reply);
         message.lineReply(embed);
     },
 };
