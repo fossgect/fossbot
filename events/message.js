4078ed5ab3b52080b9b4091c73be100e59fff257
Mr-Skully <emiltitus.inc@gmail.com>
2021-07-29 11:16:21 +0530

    Stylistic changes

--- events/message.js
+++ events/message.js
@@ -5,10 +5,11 @@ const { BOT_CHANNEL } = require("../util/botConfig");
 module.exports = {
     name: "message",
     execute(message) {
+
         // checks whether the message is a command, whether it is from the bot itself,
         // or whether it is a message in a non-bot channel
         if (!message.content.startsWith(message.client.prefix) || message.author.bot
-        || message.channel.id != BOT_CHANNEL) return;
+                || message.channel.name != BOT_CHANNEL) return;
 
         // removes the prefix and splits the message into arguments
         const args = message.content.slice(message.client.prefix.length).trim().split(/\s+/);
@@ -16,19 +17,19 @@ module.exports = {
 
         // checks whether the command exists
         const command = message.client.commands.get(commandName)
-            || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+                || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
         if (!command) return;
 
         // handle DMs to the bot
         if (message.channel.type === "dm") {
-            return message.lineReply("Can't execute commands inside DMs. Talk to me from the server.");
+            return message.lineReply("Can't execute commands inside DMs. 😅\nTalk to me from the server.");
         }
 
         // check whether the user has permission to execute the command
         if (command.permissions) {
             const authorPerms = message.channel.permissionsFor(message.author);
             if (!authorPerms || !authorPerms.has(command.permissions)) {
-                return message.lineReply("You don't have permission to do this!");
+                return message.lineReply("❌ You don't have permission to do this!");
             }
         }
 
@@ -51,7 +52,7 @@ module.exports = {
         }
         catch (error) {
             console.error(error);
-            message.reply("there was an error trying to execute that command!");
+            message.reply("❌ There was an error trying to execute that command!");
         }
     },
 };
